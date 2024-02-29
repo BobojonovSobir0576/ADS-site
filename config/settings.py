ff82f3749603d25e0bba5724a6be3e16fa24e624
BobojonovSobir0576 <dweb0987654321@gmail.com>
2024-02-29 08:39:27 +0500

    fixed settings

--- config/settings.py
+++ config/settings.py
@@ -62,9 +62,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    # 'utils.middlewares.middleware.JsonErrorResponseMiddleware',
-    # 'utils.middlewares.middleware.Custom404Middleware',
-    # 'utils.middlewares.middleware.SimpleJWTAuthenticationMiddleware',
+    'utils.middlewares.middleware.JsonErrorResponseMiddleware',
+    'utils.middlewares.middleware.Custom404Middleware',
+    'utils.middlewares.middleware.SimpleJWTAuthenticationMiddleware',
     'utils.middlewares.middleware.DisableCSRFOnAPI',
 
 ]
